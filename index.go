cd7f475cb72817ff3a71ad2cb96cbde3038c780c
DMN <dmn@DMNs-MacBook-Pro.local>
2018-12-26 23:53:41 -0600

    funciones y concatenados

--- index.go
+++ index.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 //but it comes from C++ dude
 // D: f*ckin christ dhag!
+//.....................
+
+
+func add(x int, y int) int {
+	return x + y
+}
+//variables  var  kyo int
 
 func main(){
-	fmt.Println("Yesh");
+	var kyo int  = 10
+	fmt.Println("yesh ",kyo, "\n");
+
+	//otra variable
+
+	kay := "kay"
+	fmt.Println(kay, "\n");
+
+	fmt.Println(add(42, 13))
 }
